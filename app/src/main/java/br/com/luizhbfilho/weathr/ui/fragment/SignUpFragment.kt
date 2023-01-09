af18f6e265c09122f40622b8c2b4825b864768cf
luizhbfilho <luizhbfilho@outlook.com>
2023-01-09 15:36:25 -0300

    nav_graph implementation start

--- app/src/main/java/br/com/luizhbfilho/weathr/ui/fragment/SignUpFragment.kt
+++ app/src/main/java/br/com/luizhbfilho/weathr/ui/fragment/SignUpFragment.kt
@@ -4,12 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import br.com.luizhbfilho.weathr.databinding.FragmentSignUpBinding
 import br.com.luizhbfilho.weathr.ui.viewmodel.SignUpViewModel
 import com.google.firebase.auth.FirebaseAuth
 
-class SignUpFragment: Fragment() {
+class SignUpFragment : Fragment() {
     private lateinit var auth: FirebaseAuth
     private lateinit var binding: FragmentSignUpBinding
     private lateinit var viewModel: SignUpViewModel
@@ -25,8 +26,35 @@ class SignUpFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        auth = FirebaseAuth.getInstance(
+        auth = FirebaseAuth.getInstance()
+        val email = binding.enterEmailTextInputEditText.text.toString()
+        val password = binding.createPasswordTextEditText.text.toString()
+        val confirmPassword = binding.confirmPasswordTextEditText.text.toString()
 
-        )
+        binding.signUpButton.setOnClickListener {
+            if (email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+                Toast.makeText(activity, "Please complete the fields correctly", Toast.LENGTH_SHORT)
+                    .show()
+                return@setOnClickListener
+            }
+            if (password != confirmPassword) {
+                Toast.makeText(activity, "The passwords do not match", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val user = auth.currentUser
+                        Toast.makeText(activity, "Welcome, ${user?.email}!", Toast.LENGTH_SHORT)
+                            .show()
+                    } else {
+                        Toast.makeText(
+                            activity,
+                            "Sign Up error: ${task.exception}",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
     }
 }
