acdc0cb9ea60c5dd98916af20d1d8a9667544950
luizhbfilho <luizhbfilho@outlook.com>
2023-01-09 23:52:02 -0300

    Sign In Fragment basic implementation

--- app/src/main/java/br/com/luizhbfilho/weathr/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/luizhbfilho/weathr/ui/activity/MainActivity.kt
@@ -1,40 +1,32 @@
 package br.com.luizhbfilho.weathr.ui.activity
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import br.com.luizhbfilho.weathr.R
 import br.com.luizhbfilho.weathr.databinding.ActivityMainBinding
-import com.google.android.material.snackbar.Snackbar
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        WindowCompat.setDecorFitsSystemWindows(window, false)
-        super.onCreate(savedInstanceState)
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
+        setSupportActionBar(binding.toolbar)
+        setUpNavigation()
     }
 
     private fun setUpNavigation() {
-
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
     override fun onSupportNavigateUp(): Boolean {
--- app/src/main/java/br/com/luizhbfilho/weathr/ui/fragment/SignInFragment.kt
+++ app/src/main/java/br/com/luizhbfilho/weathr/ui/fragment/SignInFragment.kt
@@ -1,5 +1,9 @@
 package br.com.luizhbfilho.weathr.ui.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import br.com.luizhbfilho.weathr.databinding.FragmentSignInBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -10,5 +14,25 @@ class SignInFragment: Fragment() {
     private val binding get() = _binding!!
     private lateinit var firebaseAuth: FirebaseAuth
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSignInBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
