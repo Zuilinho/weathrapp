ebbe6cb9a99de53f2832ca65639bbc02a206c1ae
luizhbfilho <luizhbfilho@outlook.com>
2023-01-10 16:33:45 -0300

    signIn to signUp navigation implementation

--- app/src/main/java/br/com/luizhbfilho/weathr/ui/fragment/SignInFragment.kt
+++ app/src/main/java/br/com/luizhbfilho/weathr/ui/fragment/SignInFragment.kt
@@ -5,12 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import br.com.luizhbfilho.weathr.R
 import br.com.luizhbfilho.weathr.databinding.FragmentSignInBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 
-class SignInFragment: Fragment() {
+class SignInFragment : Fragment() {
 
-    private var _binding : FragmentSignInBinding? = null
+    private var _binding: FragmentSignInBinding? = null
     private val binding get() = _binding!!
     private lateinit var firebaseAuth: FirebaseAuth
 
@@ -29,10 +33,30 @@ class SignInFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.loginButton.setOnClickListener {
+            val userEmail = binding.emailTextInputEditText.text.toString()
+            val userPassword = binding.passwordTextInputEditText.text.toString()
+            signIn(userEmail, userPassword)
+        }
+        binding.signUpButton.setOnClickListener {
+            view -> findNavController().navigate(R.id.action_SignInFragment_to_SignUpFragment)
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun signIn(userEmail: String, userPassword: String) {
+        firebaseAuth.signInWithEmailAndPassword(userEmail, userPassword)
+            .addOnCompleteListener(requireActivity(), OnCompleteListener<AuthResult> { task ->
+                if (task.isSuccessful) {
+                    findNavController().navigateUp()
+                } else {
+
+                }
+            })
     }
 }
